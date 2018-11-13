8e9e7362129237ccb14d496c20cb156783e0fb87
james.s.debettencourt <james.s.debettencourt@gmail.com>
2018-11-13 14:07:37 -0500

    end it all for IT231

---
+++ pysqlexam/functions/sqlquery.py
@@ -0,0 +1,46 @@
+import os
+import sqlite3
+import pandas as pd
+
+data_url = "./addresses.csv"
+headers = ["first_name","last_name","address","city","state","zip"]
+data_table = pd.read_csv( data_url, header=None, names=headers, converters= {'zip':str})
+
+if os.path.exists('example.db'):
+  os.remove('example.db')
+
+conn = sqlite3.connect('example.db')
+
+data_table.to_sql('data_table',conn,dtype={
+  'first_name':'VARCHAR(64)',
+  'last_name':'VARCHAR(64)',
+  'address':'VARCHAR(128)',
+  'city':'VARCHAR(64)',
+  'state':'VARCHAR(32)',
+  'zip':'VARCHAR(10)',
+})
+
+conn.row_factory = sqlite3.Row
+
+def sql_query(query):
+  cur = conn.cursor()
+  cur.execute(query)
+  rows = cur.fetchall()
+  return rows
+
+###
+###def sql_edit_insert(query,var):
+#  cur = conn.cursor()
+#  cur.execute(query,var)
+#  conn.commit()
+  
+#def sql_delete(query,var)
+#  cur = conn.cursor()
+#  cur.execute(query,var)
+  
+#def sql_query2(query,var)
+#  cur = conn.cursor()
+#  cur.execute(query,var)
+#  rows = cur.fetchall()
+#  return rows
+###
